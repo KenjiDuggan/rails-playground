a20027f2bfbc7c95d8d38dd02d0d3fe49cf54517
KenjiDuggan <kenji.duggan@shopify.com>
2024-11-19 19:53:50 -0500

    adding user scaffolding and basic authentication from built in features

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   validates :title, uniqueness: { case_sensitive: false }, presence: true
+  validates :user, presence: true
   before_validation :unique_title_on_update, on: :update
+
+  belongs_to :user
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get "home/index"
+  get "home/posts"
+  resource :session
+  resources :passwords, param: :token
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  root to: 'home#index'
+  get '/posts', to: 'home#posts'
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
